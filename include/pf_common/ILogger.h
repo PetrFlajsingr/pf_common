63dee129bdb6f4d73fb766617e9ab470905130cd
petr <xflajs00@stud.fit.vutbr.cz>
2020-11-03 18:14:04 +0100

    add name to logger

--- include/pf_common/ILogger.h
+++ include/pf_common/ILogger.h
@@ -22,16 +22,24 @@ enum class LogLevel {
 
 class ILogger {
  public:
+  inline ILogger(std::string loggerName) : name(std::move(loggerName)) {}
   template<typename... Args>
   void logFmt(LogLevel level, std::string_view format, Args &&... args) {
     log(level, fmt::format(format, std::forward<Args>(args)...));
   }
+  [[nodiscard]] virtual std::string_view getName() const {
+    return name;
+  }
   virtual void log(LogLevel level, std::string_view msg) = 0;
   virtual ~ILogger() = default;
+
+ private:
+  std::string name;
 };
 
 class EmptyLogger : public ILogger {
  public:
+  inline EmptyLogger() : ILogger("") {}
   inline void log(LogLevel, std::string_vew) override {}
 };
 
