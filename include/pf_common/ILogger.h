3859ba6cf03e9e3de02d48da55f46aef7f28920b
petr <xflajs00@stud.fit.vutbr.cz>
2020-11-03 17:40:30 +0100

    add logger interface

---
+++ include/pf_common/ILogger.h
@@ -0,0 +1,34 @@
+//
+// Created by petr on 11/3/20.
+//
+
+#ifndef PF_COMMON_INCLUDE_PF_COMMON_ILOGGER_H
+#define PF_COMMON_INCLUDE_PF_COMMON_ILOGGER_H
+
+#include <string_view>
+#include <utility>
+#include <fmt/format.h>
+
+namespace pf {
+enum class LogLevel {
+  Trace,
+  Debug,
+  Info,
+  Warn,
+  Err,
+  Critical,
+  Off
+};
+
+class ILogger {
+ public:
+  template<typename... Args>
+  void logFmt(LogLevel level, std::string_view format, Args &&... args) {
+    log(level, fmt::format(format, std::forward<Args>(args)...));
+  }
+  virtual void log(LogLevel level, std::string_view msg) = 0;
+  virtual ~ILogger() = default;
+};
+
+}// namespace pf
+#endif//PF_COMMON_INCLUDE_PF_COMMON_ILOGGER_H
