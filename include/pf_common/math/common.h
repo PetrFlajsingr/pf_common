471a1d6acbef7c17a3397cf7225897b257f23db7
petr <xflajs00@stud.fit.vutbr.cz>
2020-11-03 17:32:54 +0100

    rename math -> range, add bounding box&sphere

---
+++ include/pf_common/math/common.h
@@ -0,0 +1,12 @@
+//
+// Created by petr on 12/14/19.
+//
+
+#ifndef UTILITIES_COMMON_H
+#define UTILITIES_COMMON_H
+
+namespace pf::math {
+enum class RelativePosition { Inside, Outside, Intersection };
+}
+
+#endif // UTILITIES_COMMON_H
