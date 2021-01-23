d3236e8045b5b70ac6ac83e39a57480f15d8482e
petr <xflajs00@stud.fit.vutbr.cz>
2021-01-23 10:56:22 +0100

    add enum Enabled

--- include/pf_common/enums.h
+++ include/pf_common/enums.h
@@ -5,4 +5,10 @@
 #ifndef PF_COMMON_INCLUDE_PF_COMMON_ENUMS_H
 #define PF_COMMON_INCLUDE_PF_COMMON_ENUMS_H
 
+namespace pf{
+
+enum class Enabled { Yes, No };
+
+}
+
 #endif//PF_COMMON_INCLUDE_PF_COMMON_ENUMS_H
