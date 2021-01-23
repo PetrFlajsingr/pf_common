447cdfd3889d596ed5419c859a8066feffdb57da
petr <xflajs00@stud.fit.vutbr.cz>
2021-01-23 14:18:59 +0100

    use concept in Subscription constructor

--- include/pf_common/Subscription.h
+++ include/pf_common/Subscription.h
@@ -6,12 +6,13 @@
 #define PF_COMMON_SUBSCRIPTION_H
 
 #include <functional>
+#include <concepts>
 
 namespace pf {
 class Subscription {
  public:
   using Unsubscriber = std::function<void()>;
-  inline explicit Subscription(Unsubscriber &&unsubscriber) : unsub(unsubscriber) {}
+  inline explicit Subscription(std::invocable auto &&unsubscriber) : unsub(unsubscriber) {}
   Subscription(const Subscription &) = delete;
   Subscription &operator=(const Subscription &) = delete;
   inline Subscription(Subscription &&other) noexcept : unsub(std::move(other.unsub)) {
