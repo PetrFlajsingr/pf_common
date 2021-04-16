0d1b1b2df4c8f0cd6a06d46de9f8a8d476cfe545
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-16 13:21:17 +0200

    add docs Incrementable

--- include/pf_common/concepts/Incrementable.h
+++ include/pf_common/concepts/Incrementable.h
@@ -8,6 +8,10 @@
 #include <concepts>
 
 namespace pf {
+/**
+ * @brief A type which can use all increment operators.
+ * @tparam T
+ */
 template<typename T>
 concept Incrementable = requires(T t) {
   {++t};
@@ -15,6 +19,10 @@ concept Incrementable = requires(T t) {
   { t + t } -> std::convertible_to<T>;
 };
 
+/**
+ * @brief A type which is capable of using the modulo operator.
+ * @tparam T
+ */
 template<typename T>
 concept ModCapable = requires(T t) {
   { t % t } -> std::convertible_to<T>;
