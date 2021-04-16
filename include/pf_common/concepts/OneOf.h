0a1416c55a1132a573cf0a5070e04e30721bff3d
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-16 13:23:03 +0200

    add docs OneOf

--- include/pf_common/concepts/OneOf.h
+++ include/pf_common/concepts/OneOf.h
@@ -7,9 +7,19 @@
 
 #include <concepts>
 namespace pf {
+/**
+ * @brief Check if T is one of types provided in Args...
+ * @tparam T needle
+ * @tparam Args haystack
+ */
 template<typename T, typename... Args>
 constexpr bool OneOf_v = (std::same_as<T, Args> || ...);
 
+/**
+ * @brief A type which is one of Args...
+ * @tparam T needle
+ * @tparam Args haystack
+ */
 template<typename T, typename... Args>
 concept OneOf = (std::same_as<T, Args> || ...);
 }// namespace pf
