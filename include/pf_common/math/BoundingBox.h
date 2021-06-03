ee54dc4a1e03ad708476963b311ae838b2c15f9b
petr <xflajs00@stud.fit.vutbr.cz>
2021-06-03 16:08:08 +0200

    fix wrong code placement

--- include/pf_common/math/BoundingBox.h
+++ include/pf_common/math/BoundingBox.h
@@ -49,7 +49,42 @@ requires(OneOfValues_v<Dimensions, 2, 3>) struct BoundingBox {
    * @return true if the point is contained within the bounding box, false otherwise
    */
   [[nodiscard]] bool contains(Point point) const;
+
+
+  [[nodiscard]] float width() const { return p2.x - p1.x; }
+
+  [[nodiscard]] float height() const { return p2.y - p1.y; }
+
+  [[nodiscard]] float depth() const requires(Dimensions == 3) { return p2.z - p1.z; }
+
+  [[nodiscard]] float distance(const BoundingBox &other);
 };
+
+
+template<>
+float BoundingBox<2>::distance(const BoundingBox<3> &other) {
+  const auto outterBB = BoundingBox<2> {
+      {glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y)}, { glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y) }
+  }
+  const auto innerWidth = outterBB.width() - width() - other.width();
+  const auto innerHeight = outterBB.height() - height() - other.height();
+  const auto distance = glm::sqrt(glm::pow(innerWidth, 2) + glm::pow(innerHeight, 2));
+  return distance;
+}
+
+template<>
+float BoundingBox<3>::distance(const BoundingBox<3> &other) {
+  const auto outterBB = BoundingBox<3> {
+      {glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y), glm::min(p1.z, other.p1.z)}, {
+          glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y), glm::max(p2.z, other.p2.z)
+      }
+  }
+  const auto innerWidth = outterBB.width() - width() - other.width();
+  const auto innerHeight = outterBB.height() - height() - other.height();
+  const auto innerDepth = outterBB.depth() - depth() - other.depth();
+  const auto distance = glm::sqrt(glm::pow(innerWidth, 2) + glm::pow(innerHeight, 2) + glm::pow(innerDepth, 2));
+  return distance;
+}
 
 template<unsigned int Dims>
 std::ostream &operator<<(std::ostream &stream, const BoundingBox<Dims> &aabb);
