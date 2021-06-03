99f91fd33f07cf20f1a7ef33ede6b2036d69cad7
petr <xflajs00@stud.fit.vutbr.cz>
2021-06-03 16:15:28 +0200

    add combine function

--- include/pf_common/math/BoundingBox.h
+++ include/pf_common/math/BoundingBox.h
@@ -29,7 +29,7 @@ requires(OneOfValues_v<Dimensions, 2, 3>) struct BoundingBox {
    * @param p1 first point
    * @param p2 second point
    */
-  BoundingBox(glm::vec3 p1, glm::vec3 p2) : p1(p1), p2(p2) {}
+  BoundingBox(Point p1, Point p2) : p1(p1), p2(p2) {}
 
   bool operator==(const BoundingBox &rhs) const;
 
@@ -50,7 +50,6 @@ requires(OneOfValues_v<Dimensions, 2, 3>) struct BoundingBox {
    */
   [[nodiscard]] bool contains(Point point) const;
 
-
   [[nodiscard]] float width() const { return p2.x - p1.x; }
 
   [[nodiscard]] float height() const { return p2.y - p1.y; }
@@ -58,14 +57,14 @@ requires(OneOfValues_v<Dimensions, 2, 3>) struct BoundingBox {
   [[nodiscard]] float depth() const requires(Dimensions == 3) { return p2.z - p1.z; }
 
   [[nodiscard]] float distance(const BoundingBox &other);
+
+  [[nodiscard]] BoundingBox combine(const BoundingBox &other) const;
 };
 
-
 template<>
-float BoundingBox<2>::distance(const BoundingBox<3> &other) {
-  const auto outterBB = BoundingBox<2> {
-      {glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y)}, { glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y) }
-  }
+float BoundingBox<2>::distance(const BoundingBox<2> &other) {
+  const auto outterBB = BoundingBox<2>{{glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y)},
+                                       {glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y)}};
   const auto innerWidth = outterBB.width() - width() - other.width();
   const auto innerHeight = outterBB.height() - height() - other.height();
   const auto distance = glm::sqrt(glm::pow(innerWidth, 2) + glm::pow(innerHeight, 2));
@@ -74,16 +73,26 @@ float BoundingBox<2>::distance(const BoundingBox<3> &other) {
 
 template<>
 float BoundingBox<3>::distance(const BoundingBox<3> &other) {
-  const auto outterBB = BoundingBox<3> {
-      {glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y), glm::min(p1.z, other.p1.z)}, {
-          glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y), glm::max(p2.z, other.p2.z)
-      }
-  }
+  const auto outterBB =
+      BoundingBox<3>{{glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y), glm::min(p1.z, other.p1.z)},
+                     {glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y), glm::max(p2.z, other.p2.z)}};
   const auto innerWidth = outterBB.width() - width() - other.width();
   const auto innerHeight = outterBB.height() - height() - other.height();
   const auto innerDepth = outterBB.depth() - depth() - other.depth();
   const auto distance = glm::sqrt(glm::pow(innerWidth, 2) + glm::pow(innerHeight, 2) + glm::pow(innerDepth, 2));
   return distance;
+}
+
+template<>
+BoundingBox<2> BoundingBox<2>::combine(const BoundingBox<2> &other) const {
+  return BoundingBox<2>{{glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y)},
+                        {glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y)}};
+}
+
+template<>
+BoundingBox<3> BoundingBox<3>::combine(const BoundingBox<3> &other) const {
+  return BoundingBox<3>{{glm::min(p1.x, other.p1.x), glm::min(p1.y, other.p1.y), glm::min(p1.z, other.p1.z)},
+                        {glm::max(p2.x, other.p2.x), glm::max(p2.y, other.p2.y), glm::max(p2.z, other.p2.z)}};
 }
 
 template<unsigned int Dims>
