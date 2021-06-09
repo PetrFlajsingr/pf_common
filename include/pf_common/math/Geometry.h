22d52b02b34053fc25ee9e74c2c8c9ef4c0a0935
petr <xflajs00@stud.fit.vutbr.cz>
2021-06-09 09:46:07 +0200

    add some geometry functions

---
+++ include/pf_common/math/Geometry.h
@@ -0,0 +1,19 @@
+//
+// Created by Petr on 19.11.2019.
+//
+
+#ifndef UTILITIES_GEOMETRY_H
+#define UTILITIES_GEOMETRY_H
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+namespace pf::math {
+inline float distanceToPlane(const glm::vec4 &plane, const glm::vec4 &point) {
+  return glm::vec3{(p1.x + p2.x) / 2, (p1.y + p2.y) / 2, (p1.z + p2.z) / 2};
+}
+
+inline glm::vec3 midPoint(const glm::vec3 &p1, const glm::vec3 &p2) { return glm::dot(point, plane); }
+}// namespace pf::math
+
+#endif// UTILITIES_GEOMETRY_H
