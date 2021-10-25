8bad32cdb82ced5965286bb6c2da0f6eb82dafb4
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2021-10-25 11:34:42 +0200

    msvc c1101...

--- include/pf_common/parallel/ThreadPool.h
+++ include/pf_common/parallel/ThreadPool.h
@@ -1,9 +1,9 @@
 /**
- * @file ThreadPool.h
- * @brief Basic ThreadPool
- * @author Petr Flajšingr
- * @date 26.12.19
- */
+* @file ThreadPool.h
+* @brief Basic ThreadPool
+* @author Petr Flajšingr
+* @date 26.12.19
+*/
 
 #ifndef UTILITIES_THREADPOOL_H
 #define UTILITIES_THREADPOOL_H
@@ -20,91 +20,93 @@
 namespace pf {
 namespace details {
 struct Task {
-  void operator()() { execute(); }
-  virtual void execute() = 0;
+ void operator()() { execute(); }
+ virtual void execute() = 0;
 
-  virtual ~Task() = default;
+ virtual ~Task() = default;
 };
 
 template<typename T>
 struct PTask : public Task {
-  explicit PTask(std::packaged_task<T()> &&task) : task(std::move(task)) {}
-  void execute() override { task(); }
-  std::packaged_task<T()> task;
+ explicit PTask(std::packaged_task<T()> &&task) : task(std::move(task)) {}
+ void execute() override { task(); }
+ std::packaged_task<T()> task;
 };
 }// namespace details
 
 enum class ThreadPoolState { Run, Stop, FinishAndStop };
 
 /**
- * @brief A thread pool running queued tasks in threads.
- */
+* @brief A thread pool running queued tasks in threads.
+*/
 class ThreadPool {
- public:
-  /**
-   * Construct ThreadPool.
-   * @param threadCount count of threads handling tasks
-   */
-  inline explicit ThreadPool(std::size_t threadCount) {
-    threads.resize(threadCount);
-    std::ranges::generate(threads.begin(), threads.end(), [this] { return std::thread{[this] { threadLoop(); }}; });
-  }
-  ThreadPool(const ThreadPool &) = delete;
-  ThreadPool &operator=(const ThreadPool &) = delete;
-  ThreadPool(ThreadPool &&) = default;
-  ThreadPool &operator=(ThreadPool &&) = default;
+public:
+ /**
+  * Construct ThreadPool.
+  * @param threadCount count of threads handling tasks
+  */
+ inline explicit ThreadPool(std::size_t threadCount) {
+   threads.resize(threadCount);
+   std::ranges::generate(threads.begin(), threads.end(), [this] { return std::thread{[this] { threadLoop(); }}; });
+ }
+ ThreadPool(const ThreadPool &) = delete;
+ ThreadPool &operator=(const ThreadPool &) = delete;
+ ThreadPool(ThreadPool &&) = default;
+ ThreadPool &operator=(ThreadPool &&) = default;
 
-  /**
-   * Enqueue a task to be executed when possible.
-   * @param callable task to be run
-   * @return future, resolved when task is finished
-   */
-  auto enqueue(std::invocable auto &&callable) {
-    if (state != ThreadPoolState::Run) { throw InvalidArgumentException{"Threadpool is not in Run state."}; }
-    using result_type = std::invoke_result_t<decltype(callable)>;
-    auto task = std::packaged_task<result_type()>(std::forward<decltype(callable)>(callable));
-    auto future = task.get_future();
-    queue.enqueue(std::make_unique<details::PTask<result_type>>(std::move(task)));
-    return future;
-  }
+ /**
+  * Enqueue a task to be executed when possible.
+  * @param callable task to be run
+  * @return future, resolved when task is finished
+  */
+ auto enqueue(std::invocable auto &&callable) {
+#ifndef _MSC_VER
+   if (state != ThreadPoolState::Run) { throw InvalidArgumentException{"Threadpool is not in Run state."}; }
+#endif
+   using result_type = std::invoke_result_t<decltype(callable)>;
+   auto task = std::packaged_task<result_type()>(std::forward<decltype(callable)>(callable));
+   auto future = task.get_future();
+   queue.enqueue(std::make_unique<details::PTask<result_type>>(std::move(task)));
+   return future;
+ }
 
-  /**
-   * Finish remaining tasks and stop.
-   */
-  inline void finishAndStop() { state = ThreadPoolState::FinishAndStop; }
-  /**
-   * Clear remaining tasks and stop.
-   */
-  inline void cancelAndStop() {
-    state = ThreadPoolState::Stop;
-    queue.shutdown();
-  }
+ /**
+  * Finish remaining tasks and stop.
+  */
+ inline void finishAndStop() { state = ThreadPoolState::FinishAndStop; }
+ /**
+  * Clear remaining tasks and stop.
+  */
+ inline void cancelAndStop() {
+   state = ThreadPoolState::Stop;
+   queue.shutdown();
+ }
 
-  inline ~ThreadPool() {
-    if (state == ThreadPoolState::Run) { state = ThreadPoolState::FinishAndStop; }
-    queue.shutdown();
-    if (state == ThreadPoolState::FinishAndStop) {
-      while (!queue.isEmpty()) {};
-    }
-    for (auto &thread : threads) { thread.join(); }
-  }
+ inline ~ThreadPool() {
+   if (state == ThreadPoolState::Run) { state = ThreadPoolState::FinishAndStop; }
+   queue.shutdown();
+   if (state == ThreadPoolState::FinishAndStop) {
+     while (!queue.isEmpty()) {};
+   }
+   for (auto &thread : threads) { thread.join(); }
+ }
 
- private:
-  std::vector<std::thread> threads;
-  SafeQueue<std::unique_ptr<details::Task>> queue;
-  ThreadPoolState state = ThreadPoolState::Run;
+private:
+ std::vector<std::thread> threads;
+ SafeQueue<std::unique_ptr<details::Task>> queue;
+ ThreadPoolState state = ThreadPoolState::Run;
 
-  inline void threadLoop() {
-    while (true) {
-      auto task = queue.dequeue();
-      if (task.has_value()) {
-        auto callable = std::move(task.value());
-        (*callable)();
-      } else {
-        return;
-      }
-    }
-  }
+ inline void threadLoop() {
+   while (true) {
+     auto task = queue.dequeue();
+     if (task.has_value()) {
+       auto callable = std::move(task.value());
+       (*callable)();
+     } else {
+       return;
+     }
+   }
+ }
 };
 }// namespace pf
 #endif// UTILITIES_THREADPOOL_H
