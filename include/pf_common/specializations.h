364a64e180d4848c07002775c672510ac576549c
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2022-03-28 19:53:00 +0200

    fix derived specialization check

--- include/pf_common/specializations.h
+++ include/pf_common/specializations.h
@@ -1,9 +1,9 @@
 /**
- * @file specializations.h
- * @brief Concepts for checking if a type is specialization of another.
- * @author Petr Flajšingr
- * @date 12.4.21
- */
+* @file specializations.h
+* @brief Concepts for checking if a type is specialization of another.
+* @author Petr Flajšingr
+* @date 12.4.21
+*/
 #ifndef PF_COMMON_SPECIALIZATIONS_H
 #define PF_COMMON_SPECIALIZATIONS_H
 
@@ -18,37 +18,37 @@ template<typename T, template<class...> class U>
 constexpr static auto is_direct_specialization_v = is_direct_specialization<T, U>::value;
 
 /**
- * @brief Check if T is a direct specialization of U.
- *
- * For example static_assert(direct_specialization_of<std::vector<bool>, std::vector)
- * @tparam T
- * @tparam U
- */
+* @brief Check if T is a direct specialization of U.
+*
+* For example static_assert(direct_specialization_of<std::vector<bool>, std::vector)
+* @tparam T
+* @tparam U
+*/
 template<typename T, template<class...> class U>
 concept direct_specialization_of = is_direct_specialization_v<T, U>;
 
 namespace detail {
-  template<template<typename> class F>
-  struct conversion_tester {
-    template<typename T>
-    explicit conversion_tester(const F<T> &);
-  };
+template<template<typename> class F>
+struct conversion_tester {
+ template<typename T>
+ explicit conversion_tester(const F<T> &);
+};
 }// namespace detail
 
 template<class From, template<typename> class To>
 struct is_derived_specialization {
-  static const bool value = std::is_convertible<From, detail::conversion_tester<To>>::value;
+ static const bool value = std::is_convertible<From, detail::conversion_tester<To>>::value;
 };
 
 template<typename T, template<class...> class U>
-constexpr static auto is_derived_specialization_v = is_direct_specialization_v<T, U>;
+constexpr static auto is_derived_specialization_v = is_derived_specialization<T, U>::value;
 /**
- * @brief Check if T is a direct or derived specialization of U.
- *
- * For example static_asssert(derived_specialization_of<DerivedFromStdVector<bool>, std::vector)
- * @tparam T
- * @tparam U
- */
+* @brief Check if T is a direct or derived specialization of U.
+*
+* For example static_asssert(derived_specialization_of<DerivedFromStdVector<bool>, std::vector)
+* @tparam T
+* @tparam U
+*/
 template<typename T, template<class...> class U>
 concept derived_specialization_of = is_derived_specialization_v<T, U>;
 
