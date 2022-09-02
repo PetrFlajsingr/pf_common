0ffe251e02ad09f8deaf8ae0473c2b42fd4a6fcb
PetrFlajsingr <flajsingr@gmail.com>
2022-09-02 13:21:41 +0200

    Added `PointerLike` concept

---
+++ include/pf_common/concepts/PointerLike.h
@@ -0,0 +1,23 @@
+//
+// Created by xflajs00 on 02.09.2022.
+//
+
+#ifndef PF_COMMON_POINTERLIKE_H
+#define PF_COMMON_POINTERLIKE_H
+
+#include <concepts>
+#include <memory>
+#include <type_traits>
+
+namespace pf {
+
+template<typename T>
+concept PointerLike = std::pointer_traits<T>::element_type
+    && requires(T t) {
+         { *t } -> std::convertible_to<typename std::pointer_traits<T>::element_type>;
+         { t.operator->() } -> std::same_as<std::add_pointer<typename std::pointer_traits<T>::element_type>>;
+       };
+
+}
+
+#endif//PF_COMMON_POINTERLIKE_H
