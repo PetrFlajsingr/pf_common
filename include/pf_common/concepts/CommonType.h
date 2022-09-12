d43a7f2a3ea4a1556bf75dcda73ba33e725b2a1e
PetrFlajsingr <flajsingr@gmail.com>
2022-09-12 22:11:19 +0200

    Cleaned up `CommonType.h`

--- include/pf_common/concepts/CommonType.h
+++ include/pf_common/concepts/CommonType.h
@@ -2,18 +2,14 @@
 // Created by petr on 4/22/21.
 //
 
-#ifndef PF_COMMON_INCLUDE_PF_COMMON_CONCEPTS_COMMONTYPE_H
-#define PF_COMMON_INCLUDE_PF_COMMON_CONCEPTS_COMMONTYPE_H
+#ifndef PF_COMMON_CONCEPTS_COMMONTYPE_H
+#define PF_COMMON_CONCEPTS_COMMONTYPE_H
 
 #include <concepts>
 
 namespace pf {
 template<typename T, typename... Args>
-constexpr auto HaveCommonType_v = (std::common_with<T, Args> && ...);
+concept HaveCommonType = (std::common_with<T, Args> && ...);
+}// namespace pf
 
-
-template<typename T, typename... Args>
-concept HaveCommonType = HaveCommonType_v<T, Args...>;
-}
-
-#endif//PF_COMMON_INCLUDE_PF_COMMON_CONCEPTS_COMMONTYPE_H
+#endif//PF_COMMON_CONCEPTS_COMMONTYPE_H
