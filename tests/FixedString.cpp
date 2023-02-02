bb3843c416bef1302394cbb9e951269daf8e9124
Petr.Flajsingr <flajsingr@gmail.com>
2023-02-02 09:25:12 +0100

    Add missing include

--- tests/FixedString.cpp
+++ tests/FixedString.cpp
@@ -2,6 +2,7 @@
 // Created by petr.flajsingr on 2/7/2022.
 //
 #include <catch2/catch_test_macros.hpp>
+#include <cstring>
 #include <pf_common/FixedString.h>
 
 TEST_CASE("Fixed string", "[FixedString]") {
