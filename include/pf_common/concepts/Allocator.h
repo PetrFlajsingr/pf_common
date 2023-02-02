e3532a777590d18837f54ceaca258d1bc31d2bd2
PetrFlajsingr <flajsingr@gmail.com>
2023-02-02 19:36:49 +0100

    Add `Allocator` concept

---
+++ include/pf_common/concepts/Allocator.h
@@ -0,0 +1,25 @@
+//
+// Created by xflajs00 on 02.02.2023.
+//
+
+#ifndef PF_COMMON_ALLOCATOR_H
+#define PF_COMMON_ALLOCATOR_H
+
+#include <concepts>
+
+namespace pf {
+
+template<typename T>
+concept Allocator = requires(T t, T t2, std::size_t n, typename T::value_type *ptr) {
+  typename T::value_type;
+  { t == t2 } noexcept -> std::convertible_to<bool>;
+  { t != t2 } noexcept -> std::convertible_to<bool>;
+  { T{t} } noexcept -> std::same_as<T>;
+  { T{std::move(t)} } noexcept -> std::same_as<T>;
+  { t.allocate(n) } noexcept -> std::same_as<typename T::value_type *>;
+  { t.deallocate(ptr, n) } noexcept;
+};
+
+}
+
+#endif//PF_COMMON_ALLOCATOR_H
