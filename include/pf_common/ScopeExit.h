c7eec13b89cda0b820e8afda6d54d210d4498a1e
PetrFlajsingr <flajsingr@gmail.com>
2023-02-11 13:11:11 +0100

    Fix move behavior of `ScopeExit` Add deduction guide for `ScopeExit` Bump version

--- include/pf_common/ScopeExit.h
+++ include/pf_common/ScopeExit.h
@@ -24,13 +24,25 @@ class ScopeExit {
    * @param callable function to be called upon object's destruction
    */
   explicit ScopeExit(F &&callable) : callable(std::forward<F>(callable)) {}
+  ScopeExit(ScopeExit &&other) noexcept : callable(std::move(other.callable)) { other.active = false; }
   ~ScopeExit() { std::invoke(callable); }
 
   ScopeExit &operator=(const ScopeExit &) const = delete;
+  ScopeExit &operator=(ScopeExit &&other) noexcept {
+    if (this == &other) { return *this; }
+    callable = std::move(other.callable);
+    other.active = false;
+    return *this;
+  }
 
  private:
   PF_NOUNIQUEADDRESS F callable;
+  bool active = true;
 };
+
+template<typename F>
+ScopeExit(F) -> ScopeExit<F>;
+
 }// namespace pf
 
 #endif//PF_COMMON_SCOPEEXIT_H
